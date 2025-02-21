ee2401b70e21d91f3a3338bf13ed7080d0188670
Ujjwal <fallparker2@gmail.com>
2025-02-21 07:36:38 +0530

    Linked List initialised

--- src/ListInit.java
+++ src/ListInit.java
@@ -1,2 +1,26 @@
+import java.util.LinkedList;
+
+
 public class ListInit {
+    private final LinkedList<Integer> input;
+    private final LinkedList<Integer>output;
+    ListInit(String ip,String op){
+        input=initList(ip);
+        output=initList(op);
+    }
+    static LinkedList<Integer> initList(String data){
+        LinkedList<Integer>list=new LinkedList<>();
+        if(data.startsWith("[")&&data.endsWith("]")){
+            data=data.substring(1,data.length()-1);
+        }
+        String []element=data.split(",");
+        for(String elem:element){
+            list.add(Integer.parseInt(elem.trim()));
+        }
+        return list;
+    }
+    boolean result (){
+        return(input.equals(output));
+    }
+
 }
